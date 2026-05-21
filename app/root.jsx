f498ca807672c8f488d8797e9edff2d42d9eca7a
henrymh5 <info@kortewebdesign.de>
2026-05-21 16:02:40 +0200

    fix: Hyros Tracking embed fixed and tested e2e

--- app/root.jsx
+++ app/root.jsx
@@ -204,6 +204,17 @@ export function Layout({children}) {
           defer
           suppressHydrationWarning
         />
+                <script
+          nonce={nonce}
+          suppressHydrationWarning
+          dangerouslySetInnerHTML={{
+            __html: `var head = document.head;
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = "https://t.qiblanco.com/v1/lst/universal-script?ph=5d7ec374b760de265c8e689aea1de481d066f670ad78f9970f2c407e375dcdb6&tag=!clicked&ref_url=" + encodeURI(document.URL);
+head.appendChild(script);`,
+          }}
+        />
       </head>
       <body>
         <LoadingBar />
